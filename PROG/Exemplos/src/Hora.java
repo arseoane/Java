04ad0b567bf7261f77b48e06a9b1e34f2894267d
arseoane <arodriguezseoane@danielcastelao.org>
2026-04-14 12:36:28 +0200

    Clase Hora

---
+++ PROG/Exemplos/src/Hora.java
@@ -0,0 +1,15 @@
+public class Hora {
+    int horas;
+    int minutos;
+    int segundos;
+
+    public Hora (){
+        horas = 0;
+        minutos = 0;
+        segundos = 0;
+    }
+
+    public Hora (int horas, int minutos, int segundos){
+
+    }
+}
