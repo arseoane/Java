d8cb3b0b9834dd705980e0150f30acf3a0420fe6
arseoane <arodriguezseoane@danielcastelao.org>
2026-04-17 10:49:30 +0200

    Clase exemplosArray

--- PROG/Exemplos/src/exemplosArray.java
+++ PROG/Exemplos/src/exemplosArray.java
@@ -8,6 +8,9 @@ public class exemplosArray {
         for (int i = 0; i<5; i++)
             System.out.println("O número é: " + numeros[i]);
 
+        for (int i = 0; i<letras.length;i++)
+            System.out.println("A letra é: " + letras[i]);
+
 
     }
 }
